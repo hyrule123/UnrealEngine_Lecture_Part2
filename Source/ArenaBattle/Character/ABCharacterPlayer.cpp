8fd476df19bc80f055be8d8e1fec72657f88dadb
HWNOTE <ekdrn1@naver.com>
2024-10-10 00:43:02 +0900

    2강: Enhanced Input Component, Modifier

--- Source/ArenaBattle/Character/ABCharacterPlayer.cpp
+++ Source/ArenaBattle/Character/ABCharacterPlayer.cpp
@@ -4,6 +4,10 @@
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
+#include "InputMappingContext.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
 AABCharacterPlayer::AABCharacterPlayer()
 {
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
@@ -16,4 +20,87 @@ AABCharacterPlayer::AABCharacterPlayer()
 	//카메라는 셀카봉 끝에 붙인다(소켓 이름을 지정 시 해당 소켓 위치에 가서 붙음)
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	FollowCamera->bUsePawnControlRotation = false;
+
+	//Input: 레퍼런스로부터 가져온다.
+	{
+		static ConstructorHelpers::FObjectFinder<UInputMappingContext> InputMappintContextRef
+		(TEXT("/Script/EnhancedInput.InputMappingContext'/Game/ArenaBattle/Input/IMC_Default.IMC_Default'"));
+		check(InputMappintContextRef.Succeeded());
+		DefaultMappingContext = InputMappintContextRef.Object;
+	}
+
+
+	{
+		static ConstructorHelpers::FObjectFinder<UInputAction> MoveActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/ArenaBattle/Input/Actions/IA_Move.IA_Move'"));
+		check(MoveActionRef.Succeeded());
+		MoveAction = MoveActionRef.Object;
+	}
+
+	{
+		static ConstructorHelpers::FObjectFinder<UInputAction> JumpActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/ArenaBattle/Input/Actions/IA_Jump.IA_Jump'"));
+		check(JumpActionRef.Succeeded());
+		JumpAction = JumpActionRef.Object;
+	}
+
+	{
+		static ConstructorHelpers::FObjectFinder<UInputAction> LookActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/ArenaBattle/Input/Actions/IA_Look.IA_Look'"));
+		check(LookActionRef.Succeeded());
+		LookAction = LookActionRef.Object;
+	}
+}
+
+void AABCharacterPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//input 담당(Controller 액터)를 가져와서 만든 키매핑을 등록한다.
+	APlayerController* PlayerController = CastChecked<APlayerController>(GetController());
+	if (UEnhancedInputLocalPlayerSubsystem* SubSystem =
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>
+		(PlayerController->GetLocalPlayer()))
+	{
+		//0: 우선순위 -> 입력 충돌이 있을경우 우선순위 가 높은 입력을 우선 처리
+		SubSystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+}
+
+void AABCharacterPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//들어온 PlayerInputComponent이 EnhancedInputComponent인지 확인
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	//점프는 부모 클래스에 정의된 함수를 사용
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
+	//이동과 Look는 이 클래스에서 새로 정의한 함수를 사용
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AABCharacterPlayer::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AABCharacterPlayer::Look);
+}
+
+void AABCharacterPlayer::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FRotator Rotation = Controller->GetControlRotation();
+	//Yaw(위쪽에서 바라보는 기준 좌우회전)
+	const FRotator YawRotation = { 0, Rotation.Yaw, 0 };
+
+	//좌우값만 남은 회전행렬에서 X축 방향을 가져와서 해당 방향으로 이동
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+
+	//
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void AABCharacterPlayer::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookAxisVector.X);
+	AddControllerPitchInput(LookAxisVector.Y);
 }
--- Source/ArenaBattle/Character/ABCharacterPlayer.h
+++ Source/ArenaBattle/Character/ABCharacterPlayer.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Character/ABCharacterBase.h"
+#include "InputActionValue.h"	//UInputAction에 필요, EnhancedInput 모듈 추가 필요
 #include "ABCharacterPlayer.generated.h"
 
 /**
@@ -16,6 +17,12 @@ class ARENABATTLE_API AABCharacterPlayer : public AABCharacterBase
 public:
 	AABCharacterPlayer();
 
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 //Camera Section
 protected:
 	//셀카봉(캐릭터로부터 카메라를 떨어뜨린 채 유지시켜 주기 위한 컴포넌트)
@@ -25,4 +32,22 @@ protected:
 	//카메라
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, Meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<class UCameraComponent> FollowCamera;
+
+//Input Section
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> LookAction;
+
+	//매핑할 함수
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
 };
